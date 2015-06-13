55b4a5bde6e001eb413ccf595edb7bbabad44b37
Kris Kata <kata.krzysztof@gmail.com>
2015-06-12 19:52:01 -0700

    BNR HypnoNerd initial commit (Ch.5)

---
+++ BNRHypnoNerd/HypnoNerd/HypnoNerd/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  HypnoNerd
+//
+//  Created by Kris Kata on 6/12/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BNRHypnoNerd/HypnoNerd/HypnoNerd/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HypnoNerd
+//
+//  Created by Kris Kata on 6/12/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BNRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNRAppDelegate class]));
+    }
+}
